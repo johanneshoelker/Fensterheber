55eaf375c79b3d4eddfc00ba85ed65f215918d73
Mate <johannes.hoelker@uni-kassel.de>
2021-04-26 11:57:17 +0200

    Mechanikmodell: Simulink

--- Konstanten.m
+++ Konstanten.m
@@ -1,10 +1,13 @@
 % Mechanik
 mF  =2.3;       %kg     Masse Fensterscheibe
-ms  =0.9;       %kg     Masse Schlitten
+mS  =0.9;       %kg     Masse Schlitten
 g   =9.81;      %m/s²   Fallbeschleunigung
 da  =0.022;     %m      Durchmesser Antriebsrolle
 mueH=0.4;       %       Haftreibungskoeffizient
 mueG=0.35;      %       Gleitreibungskoeffizient
-lF  =0.375      %m      Gesamtweg des Fensters
-k   =50         %Nm/s   Dämpfung
-c   =10000      %N/m    Federsteifigkeit
+lF  =0.375;      %m      Gesamtweg des Fensters
+k   =50;         %Nm/s   Dämpfung
+c   =10000;      %N/m    Federsteifigkeit
+
+%Für Testzwecke
+FS=60;          %N
